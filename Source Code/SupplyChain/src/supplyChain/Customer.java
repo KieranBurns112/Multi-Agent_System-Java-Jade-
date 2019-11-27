23fad19390ce0e2ea9ab7a5cff5296098f965140
KieranBurns112 <kieran.burns@yahoo.com>
2019-11-27 14:43:11 +0000

    agents turning on

--- Source Code/SupplyChain/src/supplyChain/Customer.java
+++ Source Code/SupplyChain/src/supplyChain/Customer.java
@@ -2,6 +2,10 @@ package supplyChain;
 
 import jade.core.Agent;
 
-public class Customer extends Agent{
-
+public class Customer extends Agent {
+	protected void setup() {
+	
+		System.out.println("Agent "+getAID().getName()+" is active."); //Test line
+		
+	}
 }
--- Source Code/SupplyChain/src/supplyChain/Manufacturer.java
+++ Source Code/SupplyChain/src/supplyChain/Manufacturer.java
@@ -4,11 +4,14 @@ import jade.core.Agent;
 
 public class Manufacturer extends Agent{
 
-	
-	
-	//int storageCost = 5; // "w"
-	
-	// 	For the order of which to tackle the list of orders, focus on "next day turnover"
-	//using a queue and only stocking for the next day from the 1 day delivery supplier.
-	
+	protected void setup() {
+		//int storageCost = 5; // "w"
+		
+		// 	For the order of which to tackle the list of orders, focus on "next day turnover"
+		//using a queue and only stocking for the next day from the 1 day delivery supplier.
+		
+		
+		System.out.println("Agent "+getAID().getName()+" is active."); //Test line
+		
+	}
 }
